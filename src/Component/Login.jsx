f35aed521acc884db8032f991e5bb0677d3c8f17
Ashok Bhaargaw <98650291+AshokBhaargaw@users.noreply.github.com>
2023-12-17 12:54:00 +0530

    17 - Dec All good

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,60 +1,58 @@
 import React from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 export default function Login() {
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
   return (
     <>
-      <Form className="LoginForm" onSubmit={handleSubmit}>
-        <Container>
-          <Row>
-            <label className="form-label" htmlFor="firstName">
-              Email:
-            </label>
+      <Container className="mt-5">
+        <div className="m-auto border rounded p-5 w-50" style={{backgroundColor: "#F8F9FA"}}>
+          <div className="col-md-9 mb-4 pb-2 m-auto ">
             <div className="form-outline">
+              <label className="form-label" htmlFor="Username">
+                Username:
+              </label>
               <input
-                type="email"
-                id="LoginEmail"
+                type="text"
+                id="Username"
                 className="form-control form-control-lg"
               />
             </div>
-          </Row>
-          <Row>
-            <label className="form-label" htmlFor="lastName">
-              Password:
-            </label>
+          </div>
+          <div className="col-md-9 mb-4 pb-2 m-auto">
             <div className="form-outline">
+              <label className="form-label" htmlFor="password">
+                Password:
+              </label>
               <input
                 type="password"
-                id="LoginPassword"
+                id="password"
                 className="form-control form-control-lg"
               />
             </div>
-          </Row>
-          <Row className="mt-3">
-            <div>
-              <Link
-                to={"/signup"}
-                style={{
-                  display: "block",
-                  marginBottom: 20,
-                  color: "white",
-                  textDecoration: "none",
-                }}
-                className="hoverUnderline"
-              >
-                Click here to Sign up
-              </Link>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </div>
-          </Row>
-        </Container>
-      </Form>
+          </div>
+
+          <div className="mt-4 pt-2 col-md-9 m-auto">
+            <input
+              className="btn btn-primary btn-lg"
+              type="submit"
+              defaultValue="Submit"
+            />
+            <Link
+              to={"/signup"}
+              style={{
+                marginLeft: 30,
+                display: "inline",
+                textDecoration: "none",
+                color: "black",
+              }}
+              className="hoverUnderline"
+            >
+              Click here to Signup
+            </Link>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
